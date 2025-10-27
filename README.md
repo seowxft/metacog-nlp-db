9abab9dd8c098d8b85c059fc14efda7a047fc42a
Tricia Seow <62999342+seowxft@users.noreply.github.com>
2025-10-27 18:17:12 +0000

    update dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+# metacog-nlp
+
+in command window, install `node.js` (currently using v22.21.0) and `npm`
+cd to project folder, `npm i` to install dependencies
+then run `npm run dev`
+go to localhost url
--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app  = Flask(__name__)
 # -------------------------
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = config.get('Database Parameters','database_url')
+# app.config['SQLALCHEMY_DATABASE_URI'] = config.get('Database Parameters','database_url')
 
 db.init_app(app)
 CORS(app)
